57df206fae7937b0a030b6a6fca6bb4879887202
Compass <daniktherrien@gmail.com>
2023-10-17 17:50:00 -0400

    changed config connection file

--- config/config.js
+++ config/config.js
@@ -1,38 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const connectionString = process.env.JAWSDB_URL;
-// Create a Sequelize instance using the connection string
-const sequelize = new Sequelize(connectionString);
-
-// Test the database connection
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection to the database has been established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-module.exports = sequelize;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const Sequelize = require('sequelize');
-require('dotenv').config();
-
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -43,18 +11,11 @@ if (process.env.JAWSDB_URL) {
         process.env.DB_USER,
         process.env.DB_PASSWORD,
         {
-            host: 'hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+            host: 'localhost',
             dialect: 'mysql',
             port: 3306
         }
     );
 }
-async function authTest () {
-    await sequelize.authenticate();
-    console.log("JawsDB connected successfully");
-}
-
-authTest ();
-*/
 
 module.exports = sequelize;
